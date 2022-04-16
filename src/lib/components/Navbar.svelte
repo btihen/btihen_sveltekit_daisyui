6c31afc1e3d3ec689d0ffebf44fc8e7bf65f888b
Bill Tihen <btihen@gmail.com>
2022-04-16 10:47:05 +0200

    mini-visual fixes - footer line and header hamburger bigger

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -17,21 +17,23 @@
 
       <div class="navbar-start">
         <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+          <div tabindex="0" class="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 22 22" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M2 6h16M2 12h8m-8 6h16" />
             </svg>
-          </label>
+          </div>
           <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-62">
-            <li><a>Item 1</a></li>
+            <li><a href="/">Item 1</a></li>
             <li tabindex="0">
-              <a class="justify-between">
+              <a class="justify-between" href="/">
                 Parent
-                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/>
+                </svg>
               </a>
               <ul class="p-2 shadow bg-base-100">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
+                <li><a href="/">Submenu 1</a></li>
+                <li><a href="/">Submenu 2</a></li>
               </ul>
             </li>
             <li><a>Item 3</a></li>
